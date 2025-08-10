88c53b411c460f5faa49c86f6c2d78fa60825c4e
Mitra Roshanfekr <mitraroshanfekr05@gmail.com>
2025-08-11 01:41:28 +0330

    cart btns

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,6 @@
 import { useContext, useState } from "react";
 import { ItemContext } from "../context/itemsContext";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   const currentUser = JSON.parse(localStorage.getItem("currentUser")) || null;
@@ -23,13 +24,13 @@ const Cart = () => {
 
   const handleIncrement = (id) => {
     setCartData((prev) =>
-      prev.map((i) => (i.id === id ? { ...i, quantity: (i.quantity += 1) } : i))
+      prev.map((i) => (i.id === id ? { ...i, quantity: i.quantity += 1 } : i))
     );
   };
   const handleDecrement = (id) => {
     setCartData((prev) =>
       prev
-        .map((i) => (i.id === id ? { ...i, quantity: (i.quantity -= 1) } : i))
+        .map((i) => (i.id === id ? { ...i, quantity: i.quantity -= 1 } : i))
         .filter((i) => i.quantity > 0)
     );
   };
@@ -38,6 +39,13 @@ const Cart = () => {
   }
 
   const [deleteVisibleId, setDeleteVisibleIdx] = useState(null);
+
+  const getTotal= (cartData)=>{
+    return cartData.reduce((acc,cur)=>{
+      return acc + (cur.price * cur.quantity)
+    }, 0);
+
+  }
 
   return (
     <div>
@@ -95,6 +103,40 @@ const Cart = () => {
                 <div>{`$${item.price * item.quantity}`}</div>
               </div>
             ))}
+            <div className="cart-page-btns">
+              <Link to={"/"}>
+              <button className="cart-btn ">Return to Shop</button>
+              </Link>
+              <button className="cart-btn ">Update Cart</button>
+            </div>
+            <div className="cart-footer">
+              <div className="coupon-part">
+                <div className="cart-btn">
+                  <input type="text" placeholder="Coupon Code" />
+                </div>
+                  <button className="coupon-btn">Apply Coupon</button>
+              </div>
+              <div className="total-part">
+                <h3>Cart Total</h3>
+
+
+<div className="total-part-section">
+                  <span>Subtotal:</span>
+                  <span>{getTotal(cartData)}</span>
+                </div>
+                <div className="partition"></div>
+                <div className="total-part-section">
+                  <span>ُShipping:</span>
+                  <span>{"Free"}</span>
+                </div>
+                <div className="partition"></div>
+                <div className="total-part-section">
+                  <span>total:</span>
+                  <span>{getTotal(cartData)}</span>
+                </div>
+                <button className="coupon-btn cart-checkout-btn">Procees to checkout</button>
+              </div>
+            </div>
           </>
         )}
       </div>
